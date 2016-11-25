aa5b2345fcba6ed2dd70cdecbdba399375c67b10
Xiaochj <505180646@qq.com>
2016-11-25 16:20:37 +0800

    蓝牙开发

---
+++ app/src/main/java/com/xiaochj/accessibility/OnLedAccessibilityListener.java
@@ -0,0 +1,12 @@
+package com.xiaochj.accessibility;
+
+/**
+ * Created by xiaochj on 16/11/24.
+ */
+
+public interface OnLedAccessibilityListener {
+
+    void onBtNotSupportListener();
+    void onBtNotOpenListener();
+    void onWriteDialogMac();
+}

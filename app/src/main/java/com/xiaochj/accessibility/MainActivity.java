e87283b16c01633bb507603159814ca1ec9a4ba9
cjxiao <xiaochangjian@yeah.net>
2017-02-26 00:22:36 +0800

    加入服务开关按钮test

--- app/src/main/java/com/xiaochj/accessibility/MainActivity.java
+++ app/src/main/java/com/xiaochj/accessibility/MainActivity.java
@@ -1,12 +1,30 @@
 package com.xiaochj.accessibility;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout ll = new LinearLayout(this);
+        ll.setLayoutParams(new ViewGroup.LayoutParams(-1,-1));
+        Button b = new Button(this);
+        ll.addView(b);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivityForResult(intent,0);
+            }
+        });
+        setContentView(ll);
     }
 }
--- app/src/main/java/com/xiaochj/accessibility/feature/AppForWx.java
+++ app/src/main/java/com/xiaochj/accessibility/feature/AppForWx.java
@@ -29,16 +29,76 @@ public class AppForWx implements LedAccessibilityService.OnDiffAppAccEventListen
     @Override
     public void setAccessibilityEvent(int eventType, AccessibilityEvent event) {
         if (eventType == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {//一个push推送过来,调通知的event
+            Utils.LogUtil("d","event","TYPE_NOTIFICATION_STATE_CHANGED");
             setNotifyChanged(event);
         } else if (eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {//调用窗口样式改变的event
+            Utils.LogUtil("d","event","TYPE_WINDOW_STATE_CHANGED");
             getWxSaleNumber();
         } /*else if (eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {//如果没退出微信,就一直调Action_Back返回事件
             if (!event.getPackageName().toString().equalsIgnoreCase(mContext.getString(R.string.android_launcher)))
                 mService.performGlobalAction(GLOBAL_ACTION_BACK);
         }*/
         else if(eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED){
-
+            Utils.LogUtil("d","event","TYPE_WINDOW_CONTENT_CHANGED");
         }
+        else if(eventType == AccessibilityEvent.TYPE_VIEW_CLICKED){
+            Utils.LogUtil("d","event","TYPE_VIEW_CLICKED");
+        }
+        else if(eventType == AccessibilityEvent.TYPE_VIEW_SELECTED){
+            Utils.LogUtil("d","event","TYPE_VIEW_SELECTED");
+        }
+        else if(eventType == AccessibilityEvent.TYPE_VIEW_FOCUSED){
+            Utils.LogUtil("d","event","TYPE_VIEW_FOCUSED");
+        }
+        else if(eventType == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED){
+            Utils.LogUtil("d","event","TYPE_VIEW_TEXT_CHANGED");
+        }else if(eventType == AccessibilityEvent.TYPE_VIEW_HOVER_ENTER){
+            Utils.LogUtil("d","event","TYPE_VIEW_HOVER_ENTER");
+        }else if(eventType == AccessibilityEvent.TYPE_VIEW_HOVER_EXIT){
+            Utils.LogUtil("d","event","TYPE_VIEW_HOVER_EXIT");
+        }else if(eventType == AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START){
+            Utils.LogUtil("d","event","TYPE_TOUCH_EXPLORATION_GESTURE_START");
+        }else if(eventType == AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END){
+            Utils.LogUtil("d","event","TYPE_TOUCH_EXPLORATION_GESTURE_END");
+        }else if(eventType == AccessibilityEvent.TYPE_VIEW_SCROLLED){
+            Utils.LogUtil("d","event","TYPE_VIEW_SCROLLED");
+        }else if(eventType == AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED){
+            Utils.LogUtil("d","event","TYPE_VIEW_TEXT_SELECTION_CHANGED");
+        }else if(eventType == AccessibilityEvent.TYPE_ANNOUNCEMENT){
+            Utils.LogUtil("d","event","TYPE_ANNOUNCEMENT");
+        }else if(eventType == AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED){
+            Utils.LogUtil("d","event","TYPE_VIEW_ACCESSIBILITY_FOCUSED");
+        }else if(eventType == AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED){
+            Utils.LogUtil("d","event","TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED");
+        }
+        else if(eventType == AccessibilityEvent.TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY){
+            Utils.LogUtil("d","event","TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY");
+        }
+        else if(eventType == AccessibilityEvent.TYPE_GESTURE_DETECTION_START){
+            Utils.LogUtil("d","event","TYPE_GESTURE_DETECTION_START");
+        }
+        else if(eventType == AccessibilityEvent.TYPE_GESTURE_DETECTION_END){
+            Utils.LogUtil("d","event","TYPE_GESTURE_DETECTION_END");
+        }
+        else if(eventType == AccessibilityEvent.TYPE_TOUCH_INTERACTION_START){
+            Utils.LogUtil("d","event","TYPE_TOUCH_INTERACTION_START");
+        }
+        else if(eventType == AccessibilityEvent.TYPE_TOUCH_INTERACTION_END){
+            Utils.LogUtil("d","event","TYPE_TOUCH_INTERACTION_END");
+        }
+        else if(eventType == AccessibilityEvent.CONTENT_CHANGE_TYPE_UNDEFINED){
+            Utils.LogUtil("d","event","CONTENT_CHANGE_TYPE_UNDEFINED");
+        }
+        else if(eventType == AccessibilityEvent.CONTENT_CHANGE_TYPE_SUBTREE){
+            Utils.LogUtil("d","event","CONTENT_CHANGE_TYPE_SUBTREE");
+        }
+        else if(eventType == AccessibilityEvent.CONTENT_CHANGE_TYPE_TEXT){
+            Utils.LogUtil("d","event","CONTENT_CHANGE_TYPE_TEXT");
+        }
+        else if(eventType == AccessibilityEvent.CONTENT_CHANGE_TYPE_CONTENT_DESCRIPTION){
+            Utils.LogUtil("d","event","CONTENT_CHANGE_TYPE_CONTENT_DESCRIPTION");
+        }
+
     }
 
     private void setNotifyChanged(AccessibilityEvent event){
